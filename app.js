e0a7d49e9810b6e0f2c83256a5b28359be539061
Hifzhan Akraman <jan@jan-MBP.local>
2020-11-20 16:30:48 +0700

    slider belom jadi

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ function ready() {
   quantityInput();
   clickAddToCart();
   purchase();
+  // slider();
 }
+
+// function slider(){
+//   let counter = 1;
+//   setInterval(function(){
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+//     if(counter>4){
+//       counter = 1;
+//     }
+//   }, 5000);
+// }
 
 function cartSlide() {
   const shoppingCart = document.querySelector('.shopping-cart');
